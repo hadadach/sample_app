c547fe2e1af39d90399bdb38e7c4a36dfe17c2b3
hadadach <hadadach@bowdoin.edu>
2017-12-20 11:39:54 -0800

    fixing appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html "Deploying with Heroku!"
+    render html: "Deploying with Heroku!"
   end
 end
